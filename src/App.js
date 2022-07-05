0a721ecafe9c826bc30f3d6ab2f9b35b35258a41
Matiixx <mateuszcichostepski@gmail.com>
2022-07-05 17:33:12 +0200

    Import primeflex.css

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import "primereact/resources/themes/lara-light-indigo/theme.css";
 import "primereact/resources/primereact.min.css";
 import "primeicons/primeicons.css";
+import "primeflex/primeflex.css";
 
 import CurrencyExchange from "./components/CurrencyExchange";
 
