aed615dea427ee313c96d5c0865d0b45f008c69c
Matiixx <mateuszcichostepski@gmail.com>
2022-07-05 15:49:39 +0200

    Fetch exchange rates

--- src/components/CurrencyExchange.js
+++ src/components/CurrencyExchange.js
@@ -1,5 +1,25 @@
+import axios from "axios";
 import React from "react";
+import { useEffectOnce } from "../utils/useEffectOnce";
+import useLocalStorage from "../utils/useLocalStorage";
 
 export default function CurrencyExchange() {
+  const [fetchDate, setFetchDate] = useLocalStorage("fetchDate", "");
+  const [exchangeRate, setExchangeRate] = useLocalStorage("exchangeRate", "");
+
+  const getStringDate = () => {
+    let d = new Date();
+    return d.toISOString().substr(0, d.toISOString().search("T"));
+  };
+
+  useEffectOnce(async () => {
+    if (exchangeRate && fetchDate === getStringDate()) return;
+
+    const API_URL = "http://api.nbp.pl/api/exchangerates/tables/A/?format=json";
+    let res = await axios.get(API_URL);
+    setFetchDate(getStringDate());
+    setExchangeRate(res.data);
+  });
+
   return <div>Currency Exchange</div>;
 }
