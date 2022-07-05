e26833a004dd5ded8c9e9d19895d39eca8574dd7
Matiixx <mateuszcichostepski@gmail.com>
2022-07-05 16:07:11 +0200

    Delete useless state

--- src/components/CurrencyExchange.js
+++ src/components/CurrencyExchange.js
@@ -4,7 +4,6 @@ import { useEffectOnce } from "../utils/useEffectOnce";
 import useLocalStorage from "../utils/useLocalStorage";
 
 export default function CurrencyExchange() {
-  const [fetchDate, setFetchDate] = useLocalStorage("fetchDate", "");
   const [exchangeRate, setExchangeRate] = useLocalStorage("exchangeRate", "");
 
   const getStringDate = () => {
@@ -13,12 +12,14 @@ export default function CurrencyExchange() {
   };
 
   useEffectOnce(async () => {
-    if (exchangeRate && fetchDate === getStringDate()) return;
+    if (exchangeRate && exchangeRate.effectiveDate === getStringDate()) {
+      console.log(exchangeRate);
+      return;
+    }
 
     const API_URL = "http://api.nbp.pl/api/exchangerates/tables/A/?format=json";
     let res = await axios.get(API_URL);
-    setFetchDate(getStringDate());
-    setExchangeRate(res.data);
+    setExchangeRate(res.data[0].rates);
   });
 
   return <div>Currency Exchange</div>;
